f3806be9a82e4f421164baf51b275096082f6ea4
Fitra Aditya <fitra@g.pl>
2015-02-24 16:22:29 +0700

    try to fix test

--- test/index.js
+++ test/index.js
@@ -11,8 +11,9 @@ pdf2img.setOptions({ outputdir: __dirname + '/output' });
 describe('Split and covert pdf into images', function() {
   it ('Create png files', function(done) {
     this.timeout(60000);
+    console.log(input);
     pdf2img.convert(input, function(info) {
-      n = 1;
+      var n = 1;
       info.forEach(function(file) {
         file.page.should.equal(n);
         file.name.should.equal('test_' + n + '.png');
@@ -22,10 +23,11 @@ describe('Split and covert pdf into images', function() {
     });
   });
   it ('Create jpg files', function(done) {
-    this.timeout(30000);
+    this.timeout(60000);
+    console.log(input);
     pdf2img.setOptions({ type: 'jpg' });
     pdf2img.convert(input, function(info) {
-      n = 1;
+      var n = 1;
       info.forEach(function(file) {
         file.page.should.equal(n);
         file.name.should.equal('test_' + n + '.jpg');
