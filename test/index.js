58dd59e3e101e2e4a6f6db53dfd0b61fa5a937d3
Fitra Aditya <fitra@g.pl>
2017-05-14 23:58:52 +0700

    Update unit test, make sure output file exists.

--- test/index.js
+++ test/index.js
@@ -1,40 +1,62 @@
 "use strict";
 
 var fs      = require('fs');
+var path    = require('path');
+var expect  = require('chai').expect();
 var should  = require('chai').should();
 var pdf2img = require('../index.js');
 
-var input   = __dirname + '/test.pdf';
+var input   = __dirname + path.sep + 'test.pdf';
 
 pdf2img.setOptions({
-  outputdir: __dirname + '/output',
-  targetname: 'test'
+  outputdir: __dirname + path.sep + '/output',
+  outputname: 'test'
 });
 
-describe('Split and covert pdf into images', function() {
-  it ('Create png files', function(done) {
-    this.timeout(100000);
-    pdf2img.convert(input, function(err, info) {
-      var n = 1;
-      info.forEach(function(file) {
-        file.page.should.equal(n);
-        file.name.should.equal('test_' + n + '.png');
-        if (n === 3) done();
-        n++;
-      });
-    });
-  });
+describe('Split and convert pdf into images', function() {
   it ('Create jpg files', function(done) {
     this.timeout(100000);
-    pdf2img.setOptions({ type: 'jpg' });
     pdf2img.convert(input, function(err, info) {
-      var n = 1;
-      info.forEach(function(file) {
-        file.page.should.equal(n);
-        file.name.should.equal('test_' + n + '.jpg');
-        if (n === 3) done();
-        n++;
-      });
+      if (info.result !== 'success') {
+        info.result.should.equal('success');
+        done();
+      } else {
+        var n = 1;
+        info.message.forEach(function(file) {
+          file.page.should.equal(n);
+          file.name.should.equal('test_' + n + '.jpg');
+          isFileExists(file.path).should.to.be.true;
+          if (n === 3) done();
+          n++;
+        });
+      }
+    });
+  });
+  it ('Create png files', function(done) {
+    this.timeout(100000);
+    pdf2img.setOptions({ type: 'png' });
+    pdf2img.convert(input, function(err, info) {
+      if (info.result !== 'success') {
+        info.result.should.equal('success');
+        done();
+      } else {
+        var n = 1;
+        info.message.forEach(function(file) {
+          file.page.should.equal(n);
+          file.name.should.equal('test_' + n + '.png');
+          isFileExists(file.path).should.to.be.true;
+          if (n === 3) done();
+          n++;
+        });
+      }
     });
   });
 });
+
+var isFileExists = function(path) {
+  try {
+    return fs.statSync(path).isFile();
+  } catch (e) {
+    return false;
+  }
+}
